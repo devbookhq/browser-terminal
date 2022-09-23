b893c1c7feb8c66d60489062625366b0c19176f7
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2022-09-23 20:17:12 +0200

    Remove commented code

--- src/devbook.ts
+++ src/devbook.ts
@@ -3,27 +3,8 @@ import { h } from 'preact'
 import TerminalButton from './TerminalButton'
 import { createIsland } from 'preact-island'
 
-//const body = document.querySelector('body')
-//if (!body) {
-//  throw new Error('body element not found')
-//}
-
-//const shadow = body.attachShadow({ mode: 'open' })
-//const renderIn = document.createElement('div')
-//renderIn.id = 'dbk-root'
-//shadow.appendChild(renderIn)
-
-
-//const island = createIsland(TerminalButton)
-//island.render({
-//  initialProps: {},
-//  selector: '#dbk-root'
-//})
-
-
 const btn = createIsland(TerminalButton)
 btn.render({
   initialProps: { },
   selector: 'html',
-  // inline: true
 })
