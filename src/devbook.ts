ba78f27ca1d474ad939816dd21095972c8478ca2
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2022-09-23 19:34:54 +0200

    Remove codemirror css

--- src/devbook.ts
+++ src/devbook.ts
@@ -1,8 +1,25 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { h } from 'preact'
+import TerminalButton from './TerminalButton'
 import { createIsland } from 'preact-island'
 
-import TerminalButton from './TerminalButton'
+//const body = document.querySelector('body')
+//if (!body) {
+//  throw new Error('body element not found')
+//}
+
+//const shadow = body.attachShadow({ mode: 'open' })
+//const renderIn = document.createElement('div')
+//renderIn.id = 'dbk-root'
+//shadow.appendChild(renderIn)
+
+
+//const island = createIsland(TerminalButton)
+//island.render({
+//  initialProps: {},
+//  selector: '#dbk-root'
+//})
+
 
 const btn = createIsland(TerminalButton)
 btn.render({
