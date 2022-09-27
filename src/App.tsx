3268b0205d3bec6fe091a8dcf0f288ad2dec3c46
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2022-09-27 14:50:32 +0200

    Don't render UI if terminal session isn't initialized

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,12 @@ import '../styles/app.css'
 import Terminal, { Handler } from './Terminal'
 import useOnClickOutside from './useOnClickOutside'
 
-//import usePort from './usePort'
 import usePort from './PortProvider'
 
-export interface Props {
-}
 
-function App({
-}: Props) {
+function App() {
   const [size, setSize] = useState<{ rows: number, cols: number }>()
-  const { isTabActive, terminalID: terminalId, tabID: tabId } = usePort()
+  const { isTabActive, terminalID, tabID: tabId } = usePort()
   const terminalWrapperRef = useRef<HTMLDivElement | null>(null)
   const terminalRef = useRef<Handler>(null)
   const [isHidden, setIsHidden] = useState(true)
@@ -25,67 +21,73 @@ function App({
   }
 
   function onTerminalButtonClick() {
-    setIsHidden(val => !val)
-    //terminalRef?.current?.handleInput('')
-    //terminalRef.current?.handleInput('\x0C')
-    setTimeout(() => {
-      terminalRef.current?.focus()
-    }, 150)
+    setIsHidden(val => {
+      if (!val) {
+        setTimeout(() => {
+          terminalRef.current?.focus()
+        }, 150)
+      }
+      return !val
+    })
   }
 
   useOnClickOutside(terminalWrapperRef, handleClickOutside)
 
   return (
     <Fragment>
-      <div className={`
-        ${isHidden ? 'dbk-app dbk-app-hidden' : 'dbk-app'}
-      `}
-        ref={terminalWrapperRef}
-        onScroll={e => {
-          console.log('SCROLL')
-          e.preventDefault()
-          e.stopPropagation()
-          e.stopImmediatePropagation()
-          e.cancelBubble = true
-        }}
-      >
-        <div className="dbk-header">
-          <div className="dbk-header-section">
-            tabID: {tabId}
-          </div>
-          <div className="dbk-header-section">
-            terminalID: {terminalId}
-          </div>
-          <div className="dbk-header-section">
-            isTabActive: {isTabActive ? 'true' : 'false'}
-          </div>
-          <div className="dbk-header-section">
-            rows: {size?.rows}
-            {', '}
-            cols: {size?.cols}
-          </div>
-        </div>
+      {terminalID && (
+        <Fragment>
+          <div className={`
+            ${isHidden ? 'dbk-app dbk-app-hidden' : 'dbk-app'}
+          `}
+            ref={terminalWrapperRef}
+            onScroll={e => {
+              console.log('SCROLL')
+              e.preventDefault()
+              e.stopPropagation()
+              e.stopImmediatePropagation()
+              e.cancelBubble = true
+            }}
+          >
+            <div className="dbk-header">
+              <div className="dbk-header-section">
+                tabID: {tabId}
+              </div>
+              <div className="dbk-header-section">
+                terminalID: {terminalID}
+              </div>
+              <div className="dbk-header-section">
+                isTabActive: {isTabActive ? 'true' : 'false'}
+              </div>
+              <div className="dbk-header-section">
+                rows: {size?.rows}
+                {', '}
+                cols: {size?.cols}
+              </div>
+            </div>
 
-        <div className="dbk-terminal">
-          <Terminal
-            ref={terminalRef}
-            onRunningCmdChange={console.log}
-            autofocus={true}
-            onSizeChange={setSize}
-          />
-        </div>
-      </div>
+            <div className="dbk-terminal">
+              <Terminal
+                ref={terminalRef}
+                onRunningCmdChange={console.log}
+                autofocus={true}
+                onSizeChange={setSize}
+              />
+            </div>
+          </div>
 
-      <div className="dbk-btn-wrapper">
-        <button
-          className="
-            dbk-button
-          "
-          onClick={onTerminalButtonClick}
-        >
-          {`>_ (${terminalId};${isTabActive})`}
-        </button>
-      </div>
+          <div className="dbk-btn-wrapper">
+            <button
+              className="
+                dbk-button
+              "
+              onClick={onTerminalButtonClick}
+            >
+              {`>_ (${terminalID};${isTabActive})`}
+            </button>
+          </div>
+        </Fragment>
+      )}
     </Fragment>
   )
 }
